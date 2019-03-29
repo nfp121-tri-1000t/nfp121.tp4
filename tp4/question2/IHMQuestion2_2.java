8db301b7462395010f8ebd202e8d12ae61fc6f42
Raghed Idris <raghed.edriss@isae.edu.lb>
2019-03-30 00:03:01 +0300

    question2 done

--- tp4/question2/IHMQuestion2_2.java
+++ tp4/question2/IHMQuestion2_2.java
@@ -29,15 +29,103 @@ public class IHMQuestion2_2 extends JFrame {
 
         // à compléter à l'identique de la question 2_1, (du copier/coller)...
         // le bouton A a 3 observateurs jbo1, jbo2 et jbo3
-
+  boutonA.addActionListener(new ActionListener(){ 
+                    public void actionPerformed(ActionEvent ae){
+                        contenu.append("observateur jbo3: clic du bouton A\n");
+                    }
+                });
+               boutonA.addActionListener(new ActionListener(){ 
+                    public void actionPerformed(ActionEvent ae){
+                        contenu.append("observateur jbo2: clic du bouton A\n");
+                    }
+                });
+                 boutonA.addActionListener(new ActionListener(){ 
+                    public void actionPerformed(ActionEvent ae){
+                        contenu.append("observateur jbo1: clic du bouton A\n");
+                    }
+                });
+        // ActionListener obj1 = new EcouteurDeBouton();
+         
         // le bouton B a 2 observateurs jbo1 et jbo2
-
+                boutonB.addActionListener(new ActionListener(){ 
+                    public void actionPerformed(ActionEvent ae){
+                        contenu.append("observateur jbo2: clic du bouton B\n");
+                    }
+                });
+                 boutonB.addActionListener(new ActionListener(){ 
+                    public void actionPerformed(ActionEvent ae){
+                        contenu.append("observateur jbo1: clic du bouton B\n");
+                    }
+                });
         // le bouton C a 1 observateur jbo1
+                   boutonC.addActionListener(new ActionListener(){ 
+                    public void actionPerformed(ActionEvent ae){
+                        contenu.append("observateur jbo1: clic du bouton C\n");
+                    }
+                });
 
         // à compléter pour la question 2_2 (JMouseObserver)
             // le bouton A a 1 observateur jmo1
+
+          boutonA.addMouseListener(new MouseListener() {
+                 public void mousePressed(MouseEvent me) { 
+                  
+                }
+                public void mouseReleased(MouseEvent me) { 
+                
+                }
+                public void mouseEntered(MouseEvent me) { 
+                  contenu.append("observateur jmo1 : souris entree en (" + me.getX() + "," + me.getY() +")\n");
+                } 
+                public void mouseExited(MouseEvent me) { 
+             
+                }
+                public void mouseClicked(MouseEvent me) { 
+                 
+                }
+                     
+            });
+ 
+            
             // le bouton B a 1 observateur jmo2
+                 boutonB.addMouseListener(new MouseListener() {
+                 public void mousePressed(MouseEvent me) { 
+                  
+                }
+                public void mouseReleased(MouseEvent me) { 
+                
+                }
+                public void mouseEntered(MouseEvent me) { 
+                  contenu.append("observateur jmo2 : souris entree en (" + me.getX() + "," + me.getY() +")\n");
+                } 
+                public void mouseExited(MouseEvent me) { 
+             
+                }
+                public void mouseClicked(MouseEvent me) { 
+                 
+                }
+                     
+            });
+ 
             // le bouton C a 1 observateur jmo3
+                      boutonC.addMouseListener(new MouseListener() {
+                 public void mousePressed(MouseEvent me) { 
+                  
+                }
+                public void mouseReleased(MouseEvent me) { 
+                
+                }
+                public void mouseEntered(MouseEvent me) { 
+                  contenu.append("observateur jmo3 : souris entree en (" + me.getX() + "," + me.getY() +")\n");
+                } 
+                public void mouseExited(MouseEvent me) { 
+             
+                }
+                public void mouseClicked(MouseEvent me) { 
+                 
+                }
+                     
+            });
         
     }
     
